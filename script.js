410389b00021a493ada6b2b028b6432c7b018501
Bruna Brandão <bruna.costab@hotmail.com>
2022-07-08 16:57:02 -0300

    inicío do calculo

---
+++ script.js
@@ -0,0 +1,29 @@
+let n1 = prompt('Digite um número: ')
+let n2 = prompt('Digite outro número: ')
+
+n1 = Number(n1)
+n2 = Number(n2)
+
+const sum = n1 + n2
+const sub = n1 - n2
+const multi = n1 * n2
+const div = n1 / n2
+const restDiv = n1 % n2
+
+alert('Soma: ' + sum)
+alert('Subtração: ' + sub)
+alert('Multiplicação: ' + multi)
+alert('Divisão: ' + div)
+alert('Resto da divisão: ' + restDiv)
+
+if (sum % 2 == 0) {
+  alert('A soma dos 2 é : ' + sum + ' é par')
+} else {
+  alert('A soma dos 2 é : ' + sum + ' é ímpar')
+}
+
+if (n1 === n2) {
+  alert('os números inseridos são iguais')
+} else {
+  alert('os números inseridos são diferentes')
+}
